54c547f6e2e0298ed42ac0f74664b38cfe925831
Tan <tan10tvo@gmail.com>
2022-01-12 11:53:24 -0700

    components added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App" >
-    NewsMedia 
+   <h1>NewsMedia</h1>
     </div>
   );
 }
---
+++ src/components/News.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function News(){
+
+    
+    return(
+        <div>hi</div>
+    )
+}
+
+export default News;
